eb7970d1aae204d6d1f3af62be0a8cde2feebfc7
davask <contact@davaskweblimited.com>
2017-07-04 22:45:29 +0200

    Automatic update 5.6-a2.4-d8.8

--- build/dwl/init.sh
+++ build/dwl/init.sh
@@ -1,4 +1,6 @@
-#! /bin/bash
+#! /bin/sh -e
+
+cd .; cd -;
 
 dwlDir="/dwl";
 
@@ -25,4 +27,3 @@ echo ">> Apache2 initialized";
 . ${dwlDir}/sendmail.sh
 echo ">> Sendmail initialized";
 
-. ${dwlDir}/keeprunning.sh
